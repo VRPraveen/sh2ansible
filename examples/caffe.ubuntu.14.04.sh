b33e59431a77e0d94e8efc264adf7291aa4faa00
VRPraveen <78635963+VRPraveen@users.noreply.github.com>
2021-04-28 14:17:27 +0530

    Update caffe.ubuntu.14.04.sh

--- examples/caffe.ubuntu.14.04.sh
+++ examples/caffe.ubuntu.14.04.sh
@@ -1,9 +1,30 @@
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
-sudo apt-get install --no-install-recommends libboost-all-dev
-sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
-git clone https://github.com/BVLC/caffe.git
-cd caffe
-cp Makefile.config.example Makefile.config
-make all
-make test
-make runtest
+#!/bin/bash
+os=$(uname)
+echo -e $os
+RHAT_RELEASE="/etc/redhat-release"
+{
+   os_version=""
+   if [[ "$os" = "Linux" ]]; then
+      KERNEL_VERSION=$(uname -r)
+
+       # RHEL...
+      if [[ -f "$RHAT_RELEASE" ]]; then
+         os_version=$(cat $RHAT_RELEASE 2>/dev/null | awk '{printf("%s ", $0)};END{printf("\n")}' | sed 's/Red Hat Enterprise Linux Server release/RHEL/g' | cut -f1 -d'(' | sed 's/ \+$//g')
+         RHEL_VERSION=$(echo $os_version | awk '{print $2}' | cut -f1 -d'.')
+         RHEL_MINOR=$(echo $os_version | awk '{print $2}' | cut -f2 -d'.')
+         RHEL_ARCH=$(uname -i)
+         os_version="$os_version ($RHEL_ARCH)"
+      fi
+
+      # Other...
+      if [[ "$os_version" = "" ]]; then
+         os_version=$(uname -a)
+      fi
+   fi
+   if [[ "$os" = "SunOS" ]]; then
+      os_version=$(uname -r)
+   fi
+
+   os_version=$(echo $os_version | sed 's/,/ /g')
+
+}
